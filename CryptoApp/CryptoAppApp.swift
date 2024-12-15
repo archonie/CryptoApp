c93ef967d0fd482a5e4609797452d671af14c12c
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2024-12-15 20:15:01 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
