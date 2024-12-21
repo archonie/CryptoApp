a7038b21ff6790ce111ccc320c3849d60b0669b0
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2024-12-21 12:48:31 +0300

    Add LaunchScreen

---
+++ CryptoApp/Core/Launch/Views/LaunchView.swift
@@ -0,0 +1,61 @@
+//
+//  LaunchView.swift
+//  CryptoApp
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 21.12.2024.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    
+    @State private var loadingText: [String] = "Loading your portfolio...".map {String($0)}
+    @State private var showLoadingText: Bool = false
+    private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    @State private var counter: Int = 0
+    @State private var loops: Int = 0
+    @Binding var showLaunchView: Bool
+    
+    var body: some View {
+        ZStack {
+            Color.launch.background
+                .ignoresSafeArea()
+            Image("logo-transparent")
+                .frame(width: 100, height: 100)
+            ZStack {
+                if showLoadingText {
+                    HStack(spacing: 0) {
+                        ForEach(loadingText.indices) { index in
+                            Text(String(loadingText[index]))
+                                .font(.system(size: 16, weight: .heavy, design: .monospaced))
+                                .foregroundColor(Color.launch.accent)
+                                .offset(y: counter == index ? -5 : 0)
+                        }
+                    }
+                    .transition(AnyTransition.scale.animation(.easeIn))
+                }
+            }
+            .offset(y: 70)
+        }
+        .onAppear {
+            showLoadingText.toggle()
+        }
+        .onReceive(timer) { _ in
+            withAnimation(.spring()) {
+                if counter == loadingText.count - 1 {
+                    counter = 0
+                    loops += 1
+                    if loops >= 2 {
+                        showLaunchView = false
+                    }
+                } else {
+                    counter += 1
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LaunchView(showLaunchView: .constant(true))
+}
